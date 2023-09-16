465bc1c87e1bed27ce452f2585d2c67a015a5a8a
kuqmua <serezhashmakov@outlook.com>
2023-09-16 15:10:36 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
         generate_postgresql_crud_id,
     )
 )]
-pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::TokenStream {//todo check case where all option parameters is none - must be error in this case but does not check it yet - write code to check it
     proc_macro_helpers::panic_location::panic_location();
     let proc_macro_name = "GeneratePostgresqlCrud";
     let ast: syn::DeriveInput = syn::parse(input).unwrap_or_else(|_| {
@@ -1123,6 +1123,42 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             try_response_variants_path_stringified.parse::<proc_macro2::TokenStream>()
             .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {try_response_variants_path_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
         };
+        let prepare_and_execute_query_error_token_stream = {
+            let error_path_stringified = format!("{path_to_crud}{delete_name_lower_case_stringified}::{try_camel_case_stringified}{delete_name_camel_case_stringified}");
+            error_path_stringified.parse::<proc_macro2::TokenStream>()
+            .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {error_path_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+        };
+        let check_for_all_none_token_stream = {
+            let (mut none_elements, mut match_elements) = fields_named.iter().filter(|field|*field != &id_field).fold(
+                (
+                    std::string::String::default(),
+                    std::string::String::default()
+                ), |mut acc, field| {
+                    let field_ident = field.ident.clone()
+                        .unwrap_or_else(|| {
+                            panic!("{proc_macro_name_ident_stringified} field.ident is None")
+                        });
+                    acc.0.push_str(&format!("None{dot_space}"));
+                    acc.1.push_str(&format!("&self.query.{field_ident}{dot_space}"));
+                    acc
+                });
+            none_elements.pop();
+            none_elements.pop();
+            match_elements.pop();
+            match_elements.pop();
+            let none_elements_token_stream = none_elements.parse::<proc_macro2::TokenStream>()
+            .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {none_elements} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE));
+            let match_elements_token_stream = match_elements.parse::<proc_macro2::TokenStream>()
+            .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {match_elements} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE));
+            quote::quote!{
+                if let (#none_elements_token_stream) = (#match_elements_token_stream) {
+                    return #prepare_and_execute_query_response_variants_token_stream::NoParameters { 
+                        no_parameters: std::string::String::from("no parameters"), 
+                        code_occurence: crate::code_occurence_tufa_common!(),
+                    };
+                }
+            }
+        };
         quote::quote!{
             #[derive(Debug, serde::Deserialize)]
             pub struct #delete_parameters_camel_case_token_stream {
@@ -1141,6 +1177,103 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                     #(#fields_into_url_encoding_version_with_excluded_id_token_stream)*
                     #delete_query_for_url_encoding_camel_case_token_stream {
                         #(#fields_into_url_encoding_version_constract_with_excluded_id_token_stream),*
+                    }
+                }
+            }
+            impl #delete_parameters_camel_case_token_stream {
+                pub async fn #prepare_and_execute_query_token_stream(
+                    self,
+                    app_info_state: &#app_info_state_path,
+                ) -> #prepare_and_execute_query_response_variants_token_stream
+                {
+                    let query_string = {
+                        let additional_parameters = {
+                            let mut additional_parameters = std::string::String::default();
+                            let mut increment: u64 = 0;
+                            #check_for_all_none_token_stream
+                            if let Some(value) = &self.query.name {
+                                match crate::server::postgres::bind_query::BindQuery::try_increment(
+                                    value,
+                                    &mut increment
+                                ) {
+                                    Ok(_) => {
+                                        let handle = format!("name = ${increment}");
+                                        match additional_parameters.is_empty() {
+                                            true => {
+                                                additional_parameters.push_str(&handle);
+                                            },
+                                            false => {
+                                                additional_parameters.push_str(&format!(" AND {handle}"));
+                                            },
+                                        }
+                                    },
+                                    Err(e) => {
+                                        return crate::repositories_types::tufa_server::routes::api::cats::delete::TryDeleteResponseVariants::BindQuery { 
+                                            checked_add: e.into_serialize_deserialize_version(), 
+                                            code_occurence: crate::code_occurence_tufa_common!() 
+                                        };
+                                    },
+                                }
+                            }
+                            if let Some(value) = &self.query.color {
+                                match crate::server::postgres::bind_query::BindQuery::try_increment(
+                                    value,
+                                    &mut increment
+                                ) {
+                                    Ok(_) => {
+                                        let handle = format!("color = ${increment}");
+                                        match additional_parameters.is_empty() {
+                                            true => {
+                                                additional_parameters.push_str(&handle);
+                                            },
+                                            false => {
+                                                additional_parameters.push_str(&format!(" AND {handle}"));
+                                            },
+                                        }
+                                    },
+                                    Err(e) => {
+                                        return crate::repositories_types::tufa_server::routes::api::cats::delete::TryDeleteResponseVariants::BindQuery { 
+                                            checked_add: e.into_serialize_deserialize_version(), 
+                                            code_occurence: crate::code_occurence_tufa_common!() 
+                                        };
+                                    },
+                                }
+                            }
+                            additional_parameters
+                        };
+                        format!(
+                            "{} {} {} {} {additional_parameters}",
+                            crate::server::postgres::constants::DELETE_NAME,
+                            crate::server::postgres::constants::FROM_NAME,
+                            crate::repositories_types::tufa_server::routes::api::cats::CATS,
+                            crate::server::postgres::constants::WHERE_NAME
+                        )
+                    };
+                    println!("{query_string}");
+                    let binded_query = {
+                        let mut query = sqlx::query::<sqlx::Postgres>(&query_string);
+                        #check_for_all_none_token_stream //todo - repeat of check - put it up with only one occurence?
+                        if let Some(value) = self.query.name {
+                            query = crate::server::postgres::bind_query::BindQuery::bind_value_to_query(value, query);
+                        }
+                        if let Some(value) = self.query.color {
+                            query = crate::server::postgres::bind_query::BindQuery::bind_value_to_query(value, query);
+                        }
+                        query
+                    };
+                    match binded_query
+                        .execute(app_info_state.get_postgres_pool())
+                        .await
+                    {
+                        Ok(_) => {
+                            //todo - is need to return rows affected?
+                            #prepare_and_execute_query_response_variants_token_stream::Desirable(())
+                        }
+                        Err(e) => {
+                            let error = #prepare_and_execute_query_error_token_stream::from(e);
+                            #error_log_call_token_stream
+                            #prepare_and_execute_query_response_variants_token_stream::from(error)
+                        }
                     }
                 }
             }
