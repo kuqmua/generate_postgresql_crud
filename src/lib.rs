888e305ad57e62e7a39013547df2c6112ae0ad35
kuqmua <serezhashmakov@outlook.com>
2023-09-23 11:23:41 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -2080,19 +2080,27 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             update_by_id_payload_camel_case_stringified.parse::<proc_macro2::TokenStream>()
             .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {update_by_id_payload_camel_case_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
         };
-        let fields_with_excluded_id_token_stream = fields_named.iter().filter_map(|field|match field == &id_field {
-            true => None,
-            false => {
-                let field_ident = field.ident.clone()
-                    .unwrap_or_else(|| {
-                        panic!("{proc_macro_name_ident_stringified} field.ident is None")
-                    });
-                let field_type = &field.ty;
-                Some(quote::quote!{
-                    pub #field_ident: Option<#field_type>
-                })
-            },
-        });
+        let payload_token_stream = {
+            let fields_with_excluded_id_token_stream = fields_named.iter().filter_map(|field|match field == &id_field {
+                true => None,
+                false => {
+                    let field_ident = field.ident.clone()
+                        .unwrap_or_else(|| {
+                            panic!("{proc_macro_name_ident_stringified} field.ident is None")
+                        });
+                    let field_type = &field.ty;
+                    Some(quote::quote!{
+                        pub #field_ident: Option<#field_type>
+                    })
+                },
+            });
+            quote::quote!{
+                #[derive(Debug, serde_derive::Serialize, serde_derive::Deserialize)]
+                pub struct #update_by_id_payload_camel_case_token_stream {
+                    #(#fields_with_excluded_id_token_stream),*
+                }
+            }
+        };
         let prepare_and_execute_query_token_stream = {
             let update_by_id_name_lower_case_stringified = proc_macro_helpers::to_lower_snake_case::ToLowerSnakeCase::to_lower_snake_case(&update_by_id_name_camel_case_stringified);
             let prepare_and_execute_query_response_variants_token_stream = {
@@ -2427,10 +2435,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             pub struct #update_by_id_path_camel_case_token_stream {
                 pub #id_field_ident: crate::server::postgres::bigserial::Bigserial,//#id_field_type
             }
-            #[derive(Debug, serde_derive::Serialize, serde_derive::Deserialize)]
-            pub struct #update_by_id_payload_camel_case_token_stream {
-                #(#fields_with_excluded_id_token_stream),*
-            }
+            #payload_token_stream
             #prepare_and_execute_query_token_stream
         }
     };
