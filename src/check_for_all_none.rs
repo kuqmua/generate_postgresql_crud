e476684595759d16dd162eeeaaaf5b8dbc460ff2
kuqmua <serezhashmakov@outlook.com>
2023-09-23 17:29:02 +0300

    next steps

--- src/check_for_all_none.rs
+++ src/check_for_all_none.rs
@@ -39,23 +39,27 @@ pub fn check_for_all_none(
     prepare_and_execute_query_response_variants_token_stream: &proc_macro2::TokenStream,
     query_part: QueryPart
 ) -> proc_macro2::TokenStream {
-    let (mut none_elements, mut match_elements) = fields_named.iter().filter(|field|*field != id_field).fold(
-        (
-            std::string::String::default(),
-            std::string::String::default()
-        ), |mut acc, field| {
-            let field_ident = field.ident.clone()
-                .unwrap_or_else(|| {
-                    panic!("{proc_macro_name_ident_stringified} field.ident is None")
-                });
-            acc.0.push_str(&format!("None{dot_space}"));
-            acc.1.push_str(&format!("&self.{query_part}.{field_ident}{dot_space}"));
-            acc
-        });
-    none_elements.pop();
-    none_elements.pop();
-    match_elements.pop();
-    match_elements.pop();
+    let (none_elements, match_elements) = {
+        let fields_named_filtered = fields_named.iter().filter(|field|*field != id_field).collect::<Vec<&syn::Field>>();
+        let fields_named_len = fields_named_filtered.len();
+        fields_named_filtered.iter().enumerate().fold(
+            (
+                std::string::String::default(),
+                std::string::String::default()
+            ), |mut acc, (index, field)| {
+                let field_ident = field.ident.clone()
+                    .unwrap_or_else(|| {
+                        panic!("{proc_macro_name_ident_stringified} field.ident is None")
+                    });
+                let possible_dot_space = match (index + 1) == fields_named_len {
+                    true => "",
+                    false => dot_space,
+                };
+                acc.0.push_str(&format!("None{possible_dot_space}"));
+                acc.1.push_str(&format!("&self.{query_part}.{field_ident}{possible_dot_space}"));
+                acc
+            })
+    };
     let none_elements_token_stream = none_elements.parse::<proc_macro2::TokenStream>()
     .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {none_elements} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE));
     let match_elements_token_stream = match_elements.parse::<proc_macro2::TokenStream>()
