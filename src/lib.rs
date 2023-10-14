df3ff9066ec69ec4e14f3fd12506151f0dc829c5
kuqmua <serezhashmakov@outlook.com>
2023-10-14 12:44:45 +0300

    unnest for delete with body

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ mod from_log_and_return_error;
 //todo unique(meaning not primary key unique column) and nullable support
 //todo add check on max postgresql bind elements
 //todo add route name as argument for macro - generation constant + add to generation logic
+//todo make sqlx macros instead of just queries?
 #[proc_macro_derive(
     GeneratePostgresqlCrud,
     attributes(
@@ -873,6 +874,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                     )
                 }
             };
+            // println!("{query_string_token_stream}");
             let binded_query_token_stream = {
                 let current_vec_len_name_token_stream = quote::quote!{current_vec_len};
                 let element_name_token_stream = quote::quote!{element};
@@ -944,6 +946,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                     #query_name_token_stream
                 }
             };
+            // println!("{binded_query_token_stream}");
             quote::quote!{
                 impl #create_batch_parameters_camel_case_token_stream {
                     pub async fn #prepare_and_execute_query_name_token_stream(
@@ -1189,6 +1192,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                     )
                 }
             };
+            // println!("{query_string_token_stream}");
             let binded_query_token_stream = {
                 let binded_query_modifications_token_stream = fields_named.iter().filter_map(|field|match field == &id_field {
                     true => None,
@@ -1208,6 +1212,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                     query
                 }
             };
+            // println!("{binded_query_token_stream}");
             quote::quote!{
                 impl #create_parameters_camel_case_token_stream {
                     pub async fn #prepare_and_execute_query_name_token_stream(
@@ -1667,33 +1672,14 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             };
             let query_string_primary_key_some_other_none_token_stream = {
                 let handle_token_stream = {
-                    let handle_stringified = format!("\"{delete_name_stringified} {from_name_stringified} {{}} {where_name_stringified} {id_field_ident} {in_name_stringified} ({{}}){returning_id_stringified}\"");
+                    let handle_stringified = format!("\"{delete_name_stringified} {from_name_stringified} {{}} {where_name_stringified} {id_field_ident} {in_name_stringified} ({select_name_stringified} {unnest_name_stringified}($1)){returning_id_stringified}\"");
                     handle_stringified.parse::<proc_macro2::TokenStream>()
                     .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {handle_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
                 };
                 quote::quote!{
                     format!(
                         #handle_token_stream,
-                        ROUTE_NAME,
-                        {
-                            #increment_initialization_token_stream
-                            let mut additional_parameters = std::string::String::default();
-                            for element in #id_field_ident {
-                                match #crate_server_postgres_bind_query_bind_query_try_increment_token_stream(
-                                    element,
-                                    &mut increment,
-                                ) {
-                                    Ok(_) => {
-                                        additional_parameters.push_str(&format!("${increment},"));
-                                    }
-                                    Err(e) => {
-                                        return #try_delete_with_body_response_variants_token_stream::#bind_query_variant_initialization_token_stream;
-                                    }
-                                }
-                            }
-                            additional_parameters.pop();
-                            additional_parameters
-                        }
+                        ROUTE_NAME
                     )
                 }
             };
@@ -1796,9 +1782,12 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             };
             let binded_query_primary_key_some_other_none_token_stream = quote::quote!{
                 let mut query = #sqlx_query_sqlx_postgres_token_stream(&#query_string_name_token_stream);
-                for element in #id_field_ident {
-                    query = query.bind(element.clone().into_inner());//todo think about - is clone needed?
-                }
+                query = query.bind(
+                    #id_field_ident
+                    .into_iter()
+                    .map(|element| element.clone().into_inner())
+                    .collect::<Vec<#id_field_type>>()
+                );
                 query
             };
             let binded_query_token_stream = {
