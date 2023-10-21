654b041546d37f249b3ac587ae611a7b35989f64
kuqmua <serezhashmakov@outlook.com>
2023-10-21 14:55:01 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -3029,7 +3029,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
         };
         // println!("{route_handler_token_stream}");
         quote::quote!{
-            // #parameters_token_stream
+            #parameters_token_stream
             // #query_token_stream
             // #delete_query_try_from_url_encoding_error_named_token_stream
             // #impl_std_convert_try_from_delete_query_for_url_encoding_for_delete_query_token_stream
