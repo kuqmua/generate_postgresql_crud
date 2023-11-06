5fd10bed4c25263f778b1615959cf5125cc9819c
kuqmua <serezhashmakov@outlook.com>
2023-11-06 11:56:07 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -3063,7 +3063,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             #read_many_with_body_payload_try_from_read_many_with_body_payload_with_serialize_deserialize_error_named_token_stream
             #impl_std_convert_try_from_read_many_with_body_payload_with_serialize_deserialize_for_read_many_with_body_payload_token_stream
             #impl_std_convert_from_read_many_with_body_payload_for_read_many_with_body_payload_with_serialize_deserialize_token_stream
-            // #try_read_many_with_body_error_named_token_stream
+            #try_read_many_with_body_error_named_token_stream
             // #http_request_token_stream
             // #route_handler_token_stream
         }
