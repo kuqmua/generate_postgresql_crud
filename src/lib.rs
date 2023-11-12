719b65f00b3ac9998b553d312b95cdb780427076
kuqmua <serezhashmakov@outlook.com>
2023-11-12 16:21:24 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -5385,11 +5385,10 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             &delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_camel_case_stringified,
             &proc_macro_name_ident_stringified
         );
-        let delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_lower_case_token_stream = {
-            let delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_lower_case_stringified = proc_macro_helpers::to_lower_snake_case::ToLowerSnakeCase::to_lower_snake_case(&delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_camel_case_stringified);
-            delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_lower_case_stringified.parse::<proc_macro2::TokenStream>()
-            .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_lower_case_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
-        };
+        let delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_lower_case_token_stream = generate_payload_try_from_payload_with_serialize_deserialize_lower_case_token_stream(
+            &delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_camel_case_stringified,
+            &proc_macro_name_ident_stringified
+        );
         let try_delete_many_with_body_error_named_camel_case_token_stream = generate_try_error_named_token_stream(
             try_camel_case_stringified,
             delete_many_with_body_name_camel_case_stringified,
@@ -7189,4 +7188,13 @@ fn generate_payload_try_from_payload_with_serialize_deserialize_camel_case_token
 ) -> proc_macro2::TokenStream {
     payload_try_from_payload_with_serialize_deserialize_camel_case_stringified.parse::<proc_macro2::TokenStream>()
     .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {payload_try_from_payload_with_serialize_deserialize_camel_case_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+}
+
+fn generate_payload_try_from_payload_with_serialize_deserialize_lower_case_token_stream(
+    payload_try_from_payload_with_serialize_deserialize_camel_case_stringified: &str,
+    proc_macro_name_ident_stringified: &str
+) -> proc_macro2::TokenStream {
+    let payload_try_from_payload_with_serialize_deserialize_lower_case_stringified = proc_macro_helpers::to_lower_snake_case::ToLowerSnakeCase::to_lower_snake_case(&payload_try_from_payload_with_serialize_deserialize_camel_case_stringified);
+    payload_try_from_payload_with_serialize_deserialize_lower_case_stringified.parse::<proc_macro2::TokenStream>()
+    .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {payload_try_from_payload_with_serialize_deserialize_lower_case_stringified} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
 }
