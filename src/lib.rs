5859171e7c1fde35d916f8e3664dd47df9c4afcd
kuqmua <serezhashmakov@outlook.com>
2023-11-18 02:15:24 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -6392,9 +6392,9 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                 #commit_failed_variant_declaration_token_stream,
                 #query_and_rollback_failed_variant_declaration_token_stream,
                 #[tvfrr_400_bad_request]
-                DeleteManyWithBodyPayloadTryFromDeleteManyWithBodyPayloadWithSerializeDeserialize {
+                #delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_camel_case_token_stream {
                     #eo_error_occurence_attribute_token_stream
-                    delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize: DeleteManyWithBodyPayloadTryFromDeleteManyWithBodyPayloadWithSerializeDeserializeErrorNamed,
+                    #delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_lower_case_token_stream: #delete_many_with_body_payload_try_from_delete_many_with_body_payload_with_serialize_deserialize_error_named_camel_case_token_stream,
                     #code_occurence_lower_case_double_dot_space_crate_common_code_occurence_code_occurence_token_stream,
                 },
             };
