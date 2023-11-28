811df0485ec195e3d0817e13971ac0ac82271a0f
kuqmua <serezhashmakov@outlook.com>
2023-11-28 12:17:20 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -441,11 +441,11 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
     let code_occurence_lower_case_crate_code_occurence_tufa_common_macro_call_token_stream = quote::quote!{
         #code_occurence_lower_case_token_stream: #crate_code_occurence_tufa_common_macro_call_token_stream
     };
-    let eo_error_occurence_attribute_token_stream = proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::EoErrorOccurence.to_attribute_view_token_stream();//quote::quote!{#[eo_error_occurence]};
-    let eo_display_attribute_token_stream = quote::quote!{#[eo_display]};
-    let eo_display_with_serialize_deserialize_token_stream = quote::quote!{#[eo_display_with_serialize_deserialize]};
-    let eo_display_foreign_type_token_stream = quote::quote!{#[eo_display_foreign_type]};
-    let eo_vec_error_occurence_token_stream = quote::quote!{#[eo_vec_error_occurence]};
+    let eo_error_occurence_attribute_token_stream = proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::EoErrorOccurence.to_attribute_view_token_stream();
+    let eo_display_attribute_token_stream = proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::EoDisplay.to_attribute_view_token_stream();
+    let eo_display_with_serialize_deserialize_token_stream = proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::EoDisplayWithSerializeDeserialize.to_attribute_view_token_stream();
+    let eo_display_foreign_type_token_stream = proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::EoDisplayForeignType.to_attribute_view_token_stream();
+    let eo_vec_error_occurence_token_stream = proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::EoVecErrorOccurence.to_attribute_view_token_stream();
     // let value_token_stream = quote::quote! {value};
     let impl_std_convert_try_from_ident_options_for_struct_variants_token_stream = {
         column_variants
