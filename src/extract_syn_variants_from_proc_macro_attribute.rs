87bd8c5d5373e7e8829540f63350127634ab2afa
kuqmua <serezhashmakov@outlook.com>
2023-12-16 14:45:14 +0300

    next steps

--- src/extract_syn_variants_from_proc_macro_attribute.rs
+++ src/extract_syn_variants_from_proc_macro_attribute.rs
@@ -13,18 +13,18 @@ pub fn extract_syn_variants_from_proc_macro_attribute(
     match additional_http_status_codes_error_variants_attribute.path.segments.len() == 2 {
         true => {
             let first_ident = &additional_http_status_codes_error_variants_attribute.path.segments.first().unwrap_or_else(|| {
-                panic!("{proc_macro_name_camel_case_ident_stringified} additional_http_status_codes_error_variants_attribute.path.segments.get(0) is None")
+                panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} additional_http_status_codes_error_variants_attribute.path.segments.get(0) is None")
             }).ident;
             let second_ident = &additional_http_status_codes_error_variants_attribute.path.segments.last().unwrap_or_else(|| {
-                panic!("{proc_macro_name_camel_case_ident_stringified} additional_http_status_codes_error_variants_attribute.path.segments.get(0) is None")
+                panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} additional_http_status_codes_error_variants_attribute.path.segments.get(0) is None")
             }).ident;
             let additional_http_status_codes_error_variants_attribute_path = "generate_postgresql_crud::generate_postgresql_crud_additional_http_status_codes_error_variants";
             let possible_additional_http_status_codes_error_variants_attribute_path = &format!("{first_ident}::{second_ident}");
             if let false = additional_http_status_codes_error_variants_attribute_path == possible_additional_http_status_codes_error_variants_attribute_path {
-                panic!("{proc_macro_name_camel_case_ident_stringified} {possible_additional_http_status_codes_error_variants_attribute_path} is not {generate_postgresql_crud_additional_http_status_codes_error_variant_path}")
+                panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} {possible_additional_http_status_codes_error_variants_attribute_path} is not {generate_postgresql_crud_additional_http_status_codes_error_variant_path}")
             }
         },
-        false => panic!("{proc_macro_name_camel_case_ident_stringified} no {generate_postgresql_crud_additional_http_status_codes_error_variant_path} path")
+        false => panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} no {generate_postgresql_crud_additional_http_status_codes_error_variant_path} path")
     }
     let mut additional_http_status_codes_error_variants_attribute_tokens_stringified = additional_http_status_codes_error_variants_attribute.tokens.to_string();
     let additional_http_status_codes_error_variants_attribute_tokens_stringified_len = additional_http_status_codes_error_variants_attribute_tokens_stringified.len();
@@ -33,16 +33,16 @@ pub fn extract_syn_variants_from_proc_macro_attribute(
         .iter().fold(std::vec::Vec::<syn::Variant>::new(), |mut acc, element| {
             let element_derive_input: syn::DeriveInput = syn::parse(
                 element.parse::<proc_macro2::TokenStream>()
-                .unwrap_or_else(|_| panic!("{proc_macro_name_camel_case_ident_stringified} {element} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
+                .unwrap_or_else(|_| panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} {element} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
                 .into()
             ).unwrap_or_else(|e| {
-                panic!("{proc_macro_name_camel_case_ident_stringified} parse additional_http_status_codes_error_variants_attribute_tokens failed {e}");
+                panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} parse additional_http_status_codes_error_variants_attribute_tokens failed {e}");
             });
             // let element_ident = element_derive_input.ident;//todo check if error type even exists (with empty functions)
             let data_enum = if let syn::Data::Enum(data_enum) = element_derive_input.data {
         data_enum
             } else {
-                panic!("{proc_macro_name_camel_case_ident_stringified} does not work on enums!");
+                panic!("{proc_macro_name_camel_case_ident_stringified} {generate_postgresql_crud_additional_http_status_codes_error_variant_path} does not work on enums!");
             };
             for element in data_enum.variants {
                 acc.push(element);
