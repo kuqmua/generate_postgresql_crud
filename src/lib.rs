cc57119cd27dd21828a4aa5a1ac1be40cb0ec0b7
kuqmua <serezhashmakov@outlook.com>
2023-12-31 15:00:52 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -2484,9 +2484,9 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
             crate::global_variables::compile_time::project_git_info::PROJECT_GIT_INFO.project_commit,
         )
     };
-    let application_json_token_stream = quote::quote!{"application/json"};
+    let application_json_quotes_token_stream = quote::quote!{"application/json"};
     let content_type_application_json_header_addition_token_stream = quote::quote!{
-        .header(reqwest::header::CONTENT_TYPE, #application_json_token_stream)
+        .header(reqwest::header::CONTENT_TYPE, #application_json_quotes_token_stream)
     };
     let impl_axum_response_into_response_token_stream = quote::quote!{impl axum::response::IntoResponse};
     let crate_server_routes_helpers_path_extractor_error_path_value_result_extractor_token_stream = quote::quote!{crate::server::routes::helpers::path_extractor_error::PathValueResultExtractor};
@@ -3054,124 +3054,23 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                 full_additional_http_status_codes_error_variants
             )
         };
-        println!("{:#?}", type_variants_from_request_response_syn_variants);
-        let type_variants_from_request_response_syn_variants_len = type_variants_from_request_response_syn_variants.len();
-        let status_code_enums_with_from_impls_logic_token_stream = type_variants_from_request_response_syn_variants.iter().fold(
-                std::collections::HashMap::<proc_macro_helpers::attribute::Attribute, std::vec::Vec<(
-                    &syn::Ident,
-                    std::vec::Vec<(syn::Ident, proc_macro2::TokenStream)>,
-                )>>::with_capacity(type_variants_from_request_response_syn_variants_len),
-                |mut acc, element| {
-                    let variant_ident = &element.ident;
-                    let error_variant_attribute = proc_macro_helpers::attribute::Attribute::try_from(element)
-                    .unwrap_or_else(|e| {panic!("{proc_macro_name_camel_case_ident_stringified} variant {variant_ident} failed: {e}")});
-                    let fields_named = if let syn::Fields::Named(fields_named) = &element.fields {
-                        fields_named
+        let desirable_attribute = proc_macro_helpers::attribute::Attribute::Tvfrr201Created;
+        let unique_attributes = {
+            let mut value = std::vec::Vec::with_capacity(type_variants_from_request_response_syn_variants.len());
+            value.push(desirable_attribute.clone());
+            type_variants_from_request_response_syn_variants.iter().for_each(|element|{
+                let variant_ident = &element.ident;
+                let error_variant_attribute = proc_macro_helpers::attribute::Attribute::try_from(element)
+                .unwrap_or_else(|e| {panic!("{proc_macro_name_camel_case_ident_stringified} variant {variant_ident} failed: {e}")});
+                match value.contains(&error_variant_attribute) {
+                    true => (),
+                    false => {
+                        value.push(error_variant_attribute);
                     }
-                    else {
-                        panic!("{proc_macro_name_camel_case_ident_stringified} expected fields would be named");
-                    };
-                    let error_variant_fields = fields_named.named.iter().map(|field|{
-                        let field_ident = field.ident.clone().unwrap_or_else(|| panic!(
-                            "{proc_macro_name_camel_case_ident_stringified} field.ident {}",
-                            proc_macro_helpers::error_occurence::hardcode::IS_NONE_STRINGIFIED
-                        ));
-                        let field_type_with_serialize_deserialize = match field_ident == code_occurence_lower_case_stringified {
-                            true => {
-                                let code_occurence_type_token_stream = {
-                                    if let syn::Type::Path(type_path) = &field.ty {
-                                        let mut code_occurence_type_repeat_checker = false;
-                                        let code_occurence_segments_stringified_handle = type_path.path.segments.iter()
-                                        .fold(String::from(""), |mut acc, path_segment| {
-                                            let path_segment_ident = &path_segment.ident;
-                                            match *path_segment_ident == code_occurence_camel_case_stringified {
-                                                true => {
-                                                    if code_occurence_type_repeat_checker {
-                                                        panic!("{proc_macro_name_camel_case_ident_stringified} code_occurence_ident detected more than one {code_occurence_camel_case_stringified} inside type path");
-                                                    }
-                                                    acc.push_str(&path_segment_ident.to_string());
-                                                    code_occurence_type_repeat_checker = true;
-                                                },
-                                                false => acc.push_str(&format!("{path_segment_ident}::")),
-                                            }
-                                            acc
-                                        });
-                                        if !code_occurence_type_repeat_checker {
-                                            panic!("{proc_macro_name_camel_case_ident_stringified} no {code_occurence_camel_case_stringified} named field");
-                                        }
-                                        code_occurence_segments_stringified_handle.parse::<proc_macro2::TokenStream>()
-                                        .unwrap_or_else(|_| panic!("{proc_macro_name_camel_case_ident_stringified} {code_occurence_segments_stringified_handle} {}", proc_macro_helpers::global_variables::hardcode::PARSE_PROC_MACRO2_TOKEN_STREAM_FAILED_MESSAGE))
-                                    }
-                                    else {
-                                        let syn_type_path_stringified = proc_macro_helpers::error_occurence::hardcode::syn_type_path_stringified();
-                                        panic!(
-                                            "{proc_macro_name_camel_case_ident_stringified} {code_occurence_lower_case_stringified} {} {syn_type_path_stringified}",
-                                            proc_macro_helpers::error_occurence::hardcode::SUPPORTS_ONLY_STRINGIFIED
-                                        );
-                                    }
-                                };
-                                code_occurence_type_token_stream
-                            },
-                            false => {
-                                let attribute = {
-                                    let mut option_attribute = None;
-                                    field.attrs.iter().for_each(|attr|{
-                                        if let true = attr.path.segments.len() == 1 {
-                                            let error_message = format!("{proc_macro_name_camel_case_ident_stringified} two or more supported attributes!");
-                                            let attr_ident = match attr.path.segments.iter().next() {
-                                                Some(path_segment) => &path_segment.ident,
-                                                None => panic!("attr.path.segments.iter().next() is None"),
-                                            };
-                                            match {
-                                                use std::str::FromStr;
-                                                proc_macro_helpers::error_occurence::named_attribute::NamedAttribute::from_str(&attr_ident.to_string())
-                                            } {
-                                                Ok(value) => {
-                                                    if let true = option_attribute.is_some() {
-                                                        panic!("{error_message}");
-                                                    }
-                                                    else {
-                                                        option_attribute = Some(value);
-                                                    }
-                                                },
-                                                Err(_) => ()//other attributes are not for this proc_macro
-                                            }
-                                        }//other attributes are not for this proc_macro
-                                    });
-                                    option_attribute.unwrap_or_else(|| panic!(
-                                        "{proc_macro_name_camel_case_ident_stringified} option attribute {}",
-                                        proc_macro_helpers::error_occurence::hardcode::IS_NONE_STRINGIFIED
-                                    ))
-                                };
-                                let supported_container = proc_macro_helpers::error_occurence::generate_with_serialize_deserialize_version::generate_supported_container(
-                                    &field,
-                                    &proc_macro_name_camel_case_ident_stringified,
-                                );
-                                proc_macro_helpers::error_occurence::generate_with_serialize_deserialize_version::generate_field_type_with_serialize_deserialize_version(
-                                    attribute,
-                                    supported_container,
-                                    &proc_macro_name_camel_case_ident_stringified,
-                                )
-                            },
-                        };
-                        (field_ident.clone(), field_type_with_serialize_deserialize)
-                    }).collect::<Vec<(syn::Ident, proc_macro2::TokenStream)>>();
-                    let error_variant = (
-                        variant_ident,
-                        error_variant_fields
-                    );
-                    match acc.get_mut(&error_variant_attribute) {
-                        Some(value) => {
-                            value.push(error_variant);
-                        },
-                        None => {
-                            acc.insert(error_variant_attribute, vec![error_variant]);
-                        }
-                    }
-                    acc
-                },
-            );
-            println!("{}", status_code_enums_with_from_impls_logic_token_stream.len());
+                }
+            });
+            value
+        };
         let parameters_token_stream = {
             quote::quote!{
                 #derive_debug_serialize_deserialize_token_stream
@@ -3233,7 +3132,6 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
         };
         // println!("{operation_done_but_cannot_convert_uuid_wrapper_from_possible_uuid_wrapper_in_client_error_unnamed_token_stream}");
         let try_operation_error_with_middleware_error_variants_token_stream = {
-            let desirable_attribute = proc_macro_helpers::attribute::Attribute::Tvfrr201Created;
             let try_operation_response_variants_camel_case_stringified = generate_try_operation_response_variants_camel_case_stringified(
                 &try_camel_case_stringified,
                 &operation_name_camel_case_stringified,
@@ -3506,12 +3404,18 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                 }
             };
             // println!("{try_operation_token_stream}");
-            let responses_token_stream = ["f"].iter().map(|element|{
-                let status_token_stream = quote::quote!{};
-                let description_token_stream = quote::quote!{};
-                let body_token_stream = quote::quote!{};
+            let responses_token_stream = unique_attributes.iter().map(|element|{
+                let status_token_stream = element.to_status_code_token_stream();
+                let description_token_stream = element.to_status_code_description_token_stream();
+                let body_token_stream = generate_try_operation_response_variants_desirable_attribute_token_stream(
+                    &try_camel_case_stringified,
+                    &operation_name_camel_case_stringified,
+                    &response_variants_camel_case_stringified,
+                    &element,
+                    &proc_macro_name_camel_case_ident_stringified
+                );
                 quote::quote!{
-                    (status = #status_token_stream, description = #description_token_stream, body = #body_token_stream, content_type = &application_json_token_stream)
+                    (status = #status_token_stream, description = #description_token_stream, body = #body_token_stream, content_type = #application_json_quotes_token_stream)
                 }
             }).collect::<std::vec::Vec<proc_macro2::TokenStream>>();
             let swagger_open_api_token_stream = {
@@ -3522,14 +3426,9 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
                         operation_id = "api/dogs/create_many",
                         tag = "dogs",
                         responses(
-                            (status = 201, description = "Created", body = [TryCreateManyResponseVariantsTvfrr201Created], content_type = "application/json"),
-                            (status = 408, description = "RequestTimeout", body = [TryCreateManyResponseVariantsTvfrr408RequestTimeout], content_type = "application/json"),
-                            (status = 404, description = "NotFound", body = [TryCreateManyResponseVariantsTvfrr404NotFound], content_type = "application/json"),
-                            (status = 400, description = "BadRequest", body = [TryCreateManyResponseVariantsTvfrr400BadRequest], content_type = "application/json"),
-                            (status = 500, description = "InternalServerError", body = [TryCreateManyResponseVariantsTvfrr500InternalServerError], content_type = "application/json")
+                            #(#responses_token_stream),*
                         ),
-                        // request_body(content = [CreateManyPayloadElement], description = "Pet to store the database", content_type = "application/json"),
-                        request_body(content = [#operation_payload_element_camel_case_token_stream], description = "Pet to store the database", content_type = "application/json"),
+                        request_body(content = [#operation_payload_element_camel_case_token_stream], description = "Pet to store the database", content_type = #application_json_quotes_token_stream),
                     )]
                 }
             };
