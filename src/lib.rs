dacf54db54a450a5df9969382e663319d966280f
kuqmua <serezhashmakov@outlook.com>
2024-01-18 10:28:02 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -6480,7 +6480,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
         delete_many_http_request_test_token_stream
      ) = {
         let operation = Operation::DeleteMany;
-        let operation_name_upper_camel_case_stringified = operation.to_upper_camel_case();
+        let operation_name_upper_camel_case_stringified = proc_macro_helpers::naming_conventions::ToUpperCamelCase::to_upper_camel_case(&operation);
         let operation_name_snake_case_stringified = operation.to_snake_case();
         let operation_http_method = operation.http_method();
         let operation_parameters_upper_camel_case_token_stream = operation.parameters_upper_camel_case_token_stream();
