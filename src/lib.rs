252b6cc2c4abf1ab42742b84700f6e966c10d29e
kuqmua <serezhashmakov@outlook.com>
2024-01-19 21:16:12 +0300

    next steps

--- src/lib.rs
+++ src/lib.rs
@@ -6489,17 +6489,7 @@ pub fn generate_postgresql_crud(input: proc_macro::TokenStream) -> proc_macro::T
         let operation_payload_try_from_operation_payload_with_serialize_deserialize_upper_camel_case_stringified = proc_macro_helpers::naming_conventions::PayloadTryFromPayloadWithSerializeDeserializeString::payload_try_from_payload_with_serialize_deserialize_string(&operation);
         let operation_payload_try_from_operation_payload_with_serialize_deserialize_upper_camel_case_token_stream = proc_macro_helpers::naming_conventions::PayloadTryFromPayloadWithSerializeDeserializeTokenStream::payload_try_from_payload_with_serialize_deserialize_token_stream(&operation);
         let operation_payload_try_from_operation_payload_with_serialize_deserialize_snake_case_token_stream = proc_macro_helpers::naming_conventions::PayloadTryFromPayloadWithSerializeDeserializeSnakeCaseTokenStream::payload_try_from_payload_with_serialize_deserialize_snake_case_token_stream(&operation);
-        
-        
-        // generate_payload_try_from_payload_with_serialize_deserialize_snake_case_token_stream(
-        //     &operation_payload_try_from_operation_payload_with_serialize_deserialize_upper_camel_case_stringified,
-        //     &proc_macro_name_upper_camel_case_ident_stringified
-        // );
-        let try_operation_snake_case_token_stream = generate_try_operation_snake_case_token_stream(
-            &try_snake_case_stringified,
-            &operation_name_snake_case_stringified,
-            &proc_macro_name_upper_camel_case_ident_stringified,
-        );
+        let try_operation_snake_case_token_stream = proc_macro_helpers::naming_conventions::TryOperationSnakeCaseTokenStream::try_operation_snake_case_token_stream(&operation);
         let try_operation_response_variants_token_stream = generate_try_operation_response_variants_token_stream(
             try_upper_camel_case_stringified,
             &operation_name_upper_camel_case_stringified,
